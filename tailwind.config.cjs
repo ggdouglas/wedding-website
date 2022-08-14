96da1b8377e51d5fb776b7586ff865ecf37975db
Greg Douglas <GWShark0@gmail.com>
2022-08-14 16:21:22 -0400

    Add more content and responsiveness

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -11,8 +11,25 @@ module.exports = {
       fontFamily: {
         sans: ['Nunito Sans', ...defaultTheme.fontFamily.sans],
       },
+      fontSize: {
+        '3.5xl': '2rem',
+        '4.5xl': '2.625rem',
+        '5.5xl': '3.125rem',
+      },
       letterSpacing: {
-        widest: '0.15em',
+        xs: '0.25px',
+        sm: '2px',
+        md: '4px',
+        lg: '6px',
+        xl: '8px',
+      },
+      lineHeight: {
+        tight: '1.3',
+      },
+      spacing: {
+        4.5: '1.125rem',
+        13: '3.25rem',
+        18: '4.5rem',
       },
     },
   },
