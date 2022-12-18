9e583fc122f7181f74a3997d955d4b819ae391ef
Greg Douglas <GWShark0@gmail.com>
2022-12-18 17:35:30 -0500

    kinda

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
+const plugin = require('tailwindcss/plugin');
 
 module.exports = {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -32,12 +33,30 @@ module.exports = {
       lineHeight: {
         tight: '1.3',
       },
+      scale: {
+        '200': '2',
+      },
       spacing: {
         4.5: '1.125rem',
         13: '3.25rem',
         18: '4.5rem',
       },
+      transformOrigin: {
+        'center-top': 'center top',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'translate-z': (value) => ({
+            '--tw-translate-z': value,
+            transform: ` translate3d(var(--tw-translate-x), var(--tw-translate-y), var(--tw-translate-z)) rotate(var(--tw-rotate)) skewX(var(--tw-skew-x)) skewY(var(--tw-skew-y)) scaleX(var(--tw-scale-x)) scaleY(var(--tw-scale-y))`,
+          }), // this is actual CSS
+        },
+        { values: theme('translate'), supportsNegativeValues: true }
+      );
+    }),
+  ],
 };
